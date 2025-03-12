2f8f4b4e895c2500f8f9a51c3a13cbd86cbfb776
carlos <mrlrndez@gmail.com>
2025-03-12 08:50:53 -0600

    practicas

---
+++ practicas de laboratorio logica de la programacion/practica 1/app (2).js
@@ -0,0 +1,20 @@
+
+
+const Users = [
+    {
+        fullname: 'Dereck Ventura',
+        email: 'dereckventura@gmail.com',
+        password: "1234",
+        age: 17
+    },
+
+    {
+        fullname: "Francisco Pérez",
+        email: 'franperez@gmail.com',
+        password: "8765",
+        age: 23
+    }
+]
+for(let item of Users) {
+    console.log(item.fullname);
+} 
