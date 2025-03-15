e7711f0af09689f179007d4bfae6075e7e3e87b2
carlos <mrlrndez@gmail.com>
2025-03-15 02:37:08 -0600

    Add files via upload

---
+++ Practicas de logica de programacion Carlos/Practica de logica/Practica4/VehicleService.js
@@ -0,0 +1,17 @@
+
+
+
+const vehicleOnCreate = (model, color, year, isFuncional, brand) => {
+    
+    
+  if(Imodel || Icolor || lyear || lisFuncional || Ibrand) throw new Error("TIenes que enviarme todos los argumentos para poder ejecutar correctamente la funcion")
+  
+      return {
+          modelo      : model,
+          color       : color,
+          anio        : year,
+          esFuncional : itsFuncional,
+          marca       : brand
+      }
+ 
+ } 
